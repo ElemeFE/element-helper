ef414806a3ab4c5593e8fdaaa2eaa38aeff44a05
showjoy <showjoy@192.168.0.103>
2017-06-02 22:07:32 +0800

    modify update.sh

--- resources/update.sh
+++ resources/update.sh
@@ -2,9 +2,15 @@
 
 cd ./element-gh-pages
 
-git branch -D temp
-git checkout -b temp
+if [ x$1 = x ];then
+  git init
+  git remote add -t gh-pages -f origin git@github.com:ElemeFE/element.git
+  git checkout gh-pages
+else
+  git branch -D temp
+  git checkout -b temp
 
-git branch -D gh-pages
-git fetch origin gh-pages
-git checkout --track origin/gh-pages
+  git branch -D gh-pages
+  git fetch origin gh-pages
+  git checkout --track origin/gh-pages
+fi
